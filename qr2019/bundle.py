00eb83958c69cba2bbb8afcbf4a3b3f3e6349383
Mo Frank Hu <mofrankhu@gmail.com>
2019-03-03 20:16:03 +0800

    using NITER=1000, better result.
    2, 14262, 1361, 408188, 341336

--- qr2019/bundle.py
+++ qr2019/bundle.py
@@ -1,14 +1,19 @@
 import os
 from sys import argv
+import datetime
 
 script, DEBUG, THRESHOLD = argv
 
+print(datetime.datetime.now())
 os.system("python opt.py {} {} < b_lovely_landscapes.txt > b.out".format(DEBUG, THRESHOLD))
 os.system("./checker b_lovely_landscapes.txt b.out 0")
+print(datetime.datetime.now())
 os.system("python opt.py {} {} < c_memorable_moments.txt > c.out".format(DEBUG, THRESHOLD))
 os.system("./checker c_memorable_moments.txt c.out 0")
-
+print(datetime.datetime.now())
 os.system("python opt.py {} {} < d_pet_pictures.txt > d.out".format(DEBUG, THRESHOLD))
 os.system("./checker d_pet_pictures.txt d.out 0")
+print(datetime.datetime.now())
 os.system("python opt.py {} {} < e_shiny_selfies.txt > e.out".format(DEBUG, THRESHOLD))
 os.system("./checker e_shiny_selfies.txt e.out 0")
+print(datetime.datetime.now())
