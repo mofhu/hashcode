3368682ce43165b50e0f74943c27e85b37cf53c9
Mo Frank Hu <mofrankhu@gmail.com>
2019-03-04 07:27:55 +0800

    data analysis means a lot for optimization.

--- qr2019/bundle.py
+++ qr2019/bundle.py
@@ -2,18 +2,21 @@ import os
 from sys import argv
 import datetime
 
-script, DEBUG, THRESHOLD = argv
+script, case, DEBUG, NITER, THRESHOLD = argv
+if case == "b":
+    case = "< b_lovely_landscapes.txt > b.out"
+    check = "b_lovely_landscapes.txt b.out"
+elif case == "c":
+    case = "< c_memorable_moments.txt > c.out"
+    check = "c_memorable_moments.txt c.out"
+elif case == "d":
+    case = "< d_pet_pictures.txt > d.out"
+    check = "d_pet_pictures.txt d.out"
+elif case == "e":
+    case = "< e_shiny_selfies.txt > e.out"
+    check = "e_shiny_selfies.txt e.out"
 
 print(datetime.datetime.now())
-os.system("python opt.py {} {} < b_lovely_landscapes.txt > b.out".format(DEBUG, THRESHOLD))
-os.system("./checker b_lovely_landscapes.txt b.out 0")
-print(datetime.datetime.now())
-os.system("python opt.py {} {} < c_memorable_moments.txt > c.out".format(DEBUG, THRESHOLD))
-os.system("./checker c_memorable_moments.txt c.out 0")
-print(datetime.datetime.now())
-os.system("python opt.py {} {} < d_pet_pictures.txt > d.out".format(DEBUG, THRESHOLD))
-os.system("./checker d_pet_pictures.txt d.out 0")
-print(datetime.datetime.now())
-os.system("python opt.py {} {} < e_shiny_selfies.txt > e.out".format(DEBUG, THRESHOLD))
-os.system("./checker e_shiny_selfies.txt e.out 0")
+os.system("python opt.py {} {} {} {}".format(DEBUG, NITER, THRESHOLD, case))
+os.system("./checker {} 0".format(check))
 print(datetime.datetime.now())
